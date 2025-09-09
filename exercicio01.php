eb2153cbe43e0d74b9dac2cbec332291cb1f5f8c
rodrigogulka <159147126+rodrigogulka@users.noreply.github.com>
2025-09-08 21:08:25 -0300

    Add files via upload

---
+++ exercicio01.php
@@ -0,0 +1,22 @@
+/******************************************************************************
+Curso: Engenharia de Software
+Disciplina: Linguagem e Técnicas de Programacão
+Professor: Flores
+Turma: ESOFT-2B
+Componentes:
+ 25178060-2 - Rodrigo Samuel Gulka
+Data: 8 de Setembro de 2025
+Descritivo: Escreva um programa que calcule a soma de dois números
+*******************************************************************************/
+
+<?php
+// Define dois números
+$num1 = 10;
+$num2 = 25;
+
+// Calcula a soma
+$soma = $num1 + $num2;
+
+// Exibe o resultado
+echo "A soma de $num1 + $num2 = $soma\n";
+?>
