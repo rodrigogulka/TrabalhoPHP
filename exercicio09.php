82088ed3dde438566f8a3d4a7cf983a91174ac5f
rodrigogulka <159147126+rodrigogulka@users.noreply.github.com>
2025-09-10 21:30:09 -0300

    fix

--- exercicio09.php
+++ exercicio09.php
@@ -6,6 +6,7 @@ Turma: ESOFT-2B
 Componentes:
  25178060-2 - Rodrigo Samuel Gulka
  25147198-2 - Lucas Pavelski Tanck
+ 25363232-2 - Helamã Sampaio
 Data: 8 de Setembro de 2025
 Descritivo: Faça um programa que calcule o aumento de 15% sobre um valor
 *******************************************************************************/
@@ -23,4 +24,5 @@ $valor_final = $valor_inicial + $aumento;
 echo "O valor inicial é R$ $valor_inicial\n";
 echo "O aumento de $aumento_percentual% é R$ $aumento\n";
 echo "O valor final após o aumento é R$ $valor_final\n";
+
 ?>
