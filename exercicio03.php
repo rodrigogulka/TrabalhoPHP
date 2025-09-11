b9f0288fb53d7748ecd1a38c182590eaa53ad0e3
rodrigogulka <159147126+rodrigogulka@users.noreply.github.com>
2025-09-10 21:28:44 -0300

    fix

--- exercicio03.php
+++ exercicio03.php
@@ -6,6 +6,7 @@ Turma: ESOFT-2B
 Componentes:
  25178060-2 - Rodrigo Samuel Gulka
  25147198-2 - Lucas Pavelski Tanck
+ 25363232-2 - Helamã Sampaio
 Data: 8 de Setembro de 2025
 Descritivo: Faça um programa que multiplique 8 por 7 e divida por 2
 *******************************************************************************/
@@ -24,4 +25,5 @@ $divisao = $multiplicacao / $num3;
 // Exibe o resultado
 echo "A multiplicação de $num1 * $num2 = $multiplicacao\n";
 echo "A divisão de $multiplicacao / $num3 = $divisao\n";
+
 ?>
