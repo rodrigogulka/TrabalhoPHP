e30ecf4e72e005f9d261e288467267cc9e5d8429
rodrigogulka <159147126+rodrigogulka@users.noreply.github.com>
2025-09-10 21:29:33 -0300

    fix

--- exercicio06.php
+++ exercicio06.php
@@ -6,6 +6,7 @@ Turma: ESOFT-2B
 Componentes:
  25178060-2 - Rodrigo Samuel Gulka
  25147198-2 - Lucas Pavelski Tanck
+ 25363232-2 - Helamã Sampaio
 Data: 8 de Setembro de 2025
 Descritivo: Faça um script que calcule a média de 3 números
 *******************************************************************************/
@@ -20,4 +21,5 @@ $media = ($num1 + $num2 + $num3) / 3;
 
 // Exibe o resultado
 echo "A média de $num1, $num2 e $num3 = $media\n";
+
 ?>
