519e0f0fa6bb632dfe89275bd77d0b987a716c21
rodrigogulka <159147126+rodrigogulka@users.noreply.github.com>
2025-09-10 21:30:23 -0300

    fix

--- exercicio10.php
+++ exercicio10.php
@@ -6,6 +6,7 @@ Turma: ESOFT-2B
 Componentes:
  25178060-2 - Rodrigo Samuel Gulka
  25147198-2 - Lucas Pavelski Tanck
+ 25363232-2 - Helamã Sampaio
 Data: 8 de Setembro de 2025
 Descritivo: Escreva um script que converta minutos em horas e minutos
 *******************************************************************************/
@@ -19,3 +20,4 @@ $minutos = $total_minutos % 60;
 // Exibe o resultado
 echo "$total_minutos minutos equivalem a $horas horas e $minutos minutos.\n";
 ?>
+
